af2a6fa8808225a45a7e458e44cf5fe7ea2b1fea
Saad Ahmad <ahmadsaad58@gmail.com>
2025-05-25 17:48:18 -0400

    move some functions around

--- bank/api/utilities.py
+++ bank/api/utilities.py
@@ -17,3 +17,42 @@ def check_account_name_to_username_cache() -> str:
             description: A JSON response containing the account name to username mapping.
     """
     return jsonify(ACCOUNT_NAME_TO_USERNAME), 200
+
+
+@utilities_bp.route("/", methods=["GET"])
+def index() -> str:
+    """Welcome message for the API.
+    ---
+    tags:
+      - Home
+    responses:
+        200:
+            description: A welcome message for the API.
+    """
+    return "Welcome to the Simple Bank API!"
+
+@utilities_bp.route("/health", methods=["GET"])
+def health_check() -> str:
+    """Health check endpoint to verify the API is running.
+    ---
+    tags:
+      - Health Check
+    responses:
+        200:
+            description: A message indicating the API is healthy.
+        503:
+            description: Service Unavailable if the API is not healthy.
+    """
+    return "API is healthy!", 200
+
+@utilities_bp.route("/ping", methods=["GET"])
+def ping() -> str:
+    """Ping endpoint to check if the API is responsive.
+    ---
+    tags:
+      - Ping
+    responses:
+        200:
+            description: A message indicating the API is responsive.
+    """
+    return "Pong!", 200
--- bank/app.py
+++ bank/app.py
@@ -25,46 +25,6 @@ app.register_blueprint(transactions_bp)
 app.register_blueprint(utilities_bp)
 
 
-@app.route("/", methods=["GET"])
-def index() -> str:
-    """Welcome message for the API.
-    ---
-    tags:
-      - Home
-    responses:
-        200:
-            description: A welcome message for the API.
-    """
-    return "Welcome to the Simple Bank API!"
-
-@app.route("/health", methods=["GET"])
-@limiter.limit("10 per second")
-def health_check() -> str:
-    """Health check endpoint to verify the API is running.
-    ---
-    tags:
-      - Health Check
-    responses:
-        200:
-            description: A message indicating the API is healthy.
-        503:
-            description: Service Unavailable if the API is not healthy.
-    """
-    return "API is healthy!", 200
-
-@app.route("/ping", methods=["GET"])
-def ping() -> str:
-    """Ping endpoint to check if the API is responsive.
-    ---
-    tags:
-      - Ping
-    responses:
-        200:
-            description: A message indicating the API is responsive.
-    """
-    return "Pong!", 200
-
-
 # --- Run the application ---
 if __name__ == "__main__":
     # When running in development, you can enable debug mode
